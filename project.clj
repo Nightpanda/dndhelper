b72938673af8e1879adae67cf2234d2800106a1c
Pekka Tiilikainen <tiilikap@gmail.com>
2017-11-20 07:29:50 +0200

    Adds main namespace to project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
  (defproject ddlevelup "0.1.0-SNAPSHOT"
    :description "FIXME: write description"
-   :dependencies [[org.clojure/clojure "1.8.0"]])
+   :dependencies [[org.clojure/clojure "1.8.0"]]
+   :main ddlevelup.core)
